76cb8d5663156245b808f2c8b2217ef1b42d765b
Ricardo Calixto <rcalixtos@gmail.com>
2019-02-24 17:05:04 -0300

    queue, template de e-mail, solicitação de compra, CRUD, paginação, require-dir

---
+++ src/config/mailConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'f2065aa3632d98',
+    pass: '979709806a5348'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,25 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+// ANTES DE USAR require-dir
+// const UserController = require('./app/controllers/UserController')
+// const SessionController = require('./app/controllers/SessionController')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+// DEPOIS DE USAR require-dir
+const controllers = require('./app/controllers')
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: 'true' }))
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware) // toda rota a partir daqui pra baixo faz a verificação de autenticação do usuário
+// Ads
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update) // put é usado quando queremos indicar uma atualização
+routes.delete('/ads/:id', controllers.AdController.destroy)
+
+// Purchases
+routes.post('/purchases', controllers.PurchaseController.store)
 
 module.exports = routes
